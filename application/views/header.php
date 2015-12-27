c0872a4b2ac6d0a327b55e6a70c1cd9e2d0075ae
HVish <vishnusingh1729@gmail.com>
2015-12-27 16:47:05 +0530

    added login pannel

---
+++ application/views/header.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+		<link rel="stylesheet" href="<?php echo base_url()?>css/styles.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+			<nav class="navbar navbar-default">
+				<div class="container-fluid">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span> 
+						</button>
+						<a class="navbar-brand" href="#">IChat</a>
+					</div>
+					<div class="collapse navbar-collapse" id="myNavbar">
+						<ul class="nav navbar-nav">
+							<li class="active"><a href="#">Home</a></li>
+							<li><a href="#">Page 1</a></li>
+							<li><a href="#">Page 2</a></li> 
+							<li><a href="#">Page 3</a></li> 
+						</ul>
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+							<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+						</ul>
+					</div>
+				</div>
+			</nav>
---
+++ application/views/home_view.php
@@ -0,0 +1,29 @@
+<div class="container">
+	<div class="row">
+		<div class="col-sm-4"></div>
+		<div class="col-sm-4">
+			<div class="panel panel-primary">
+				<div class="panel-heading">
+					<h4>Log In</h4>
+				</div>
+				<div class="panel-body">
+					<form action="" method="post" role="form">
+						<div class="form-group">
+							<label for="user">Username</label>
+							<input type="text" name="user" class="form-control" placeholder="Enter Username">
+						</div>
+						<div class="form-group">
+							<label for="pass">Password</label>
+							<input type="password" name="pass" class="form-control" placeholder="Enter your password">
+						</div>
+						<div class="form-group">
+							<input type="submit" class="btn btn-md btn-default">
+						</div>
+					</form>
+					<a href="signup.php">Signup here</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-4"></div>
+	</div>
+</div>
