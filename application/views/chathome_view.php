f1170effc915057dec33ab4d10472a0676653a41
HVish <vishnusingh1729@gmail.com>
2015-12-27 19:50:16 +0530

    added chathome

--- application/views/chathome_view.php
+++ application/views/chathome_view.php
@@ -2,4 +2,71 @@
 	<div class="well">
 		<h3>Hello <strong><?php echo $username;?></strong></h3>
 	</div>
+	<div class="row userbody">
+		<div class="col-sm-3 panel panel-primary ">
+			<div class="panel-heading">
+				<h3>Messages</h3>
+			</div>
+			<div class="panel-body list-group message">
+				<a class="list-group-item"><span class="badge">12</span> New</a>
+				<a class="list-group-item"><span class="badge">5</span> Deleted</a> 
+				<a class="list-group-item"><span class="badge">3</span> Warnings</a> 
+				<a class="list-group-item"><span class="badge">12</span> New</a>
+				<a class="list-group-item"><span class="badge">5</span> Deleted</a> 
+				<a class="list-group-item"><span class="badge">3</span> Warnings</a> 
+				<a class="list-group-item"><span class="badge">12</span> New</a>
+				<a class="list-group-item"><span class="badge">5</span> Deleted</a> 
+				<a class="list-group-item"><span class="badge">3</span> Warnings</a> 
+				<a class="list-group-item"><span class="badge">12</span> New</a>
+				<a class="list-group-item"><span class="badge">5</span> Deleted</a> 
+				<a class="list-group-item"><span class="badge">3</span> Warnings</a>
+				<a class="list-group-item"><span class="badge">12</span> New</a>
+				<a class="list-group-item"><span class="badge">5</span> Deleted</a> 
+				<a class="list-group-item"><span class="badge">3</span> Warnings</a> 
+			</div>
+		</div>
+		<div class="col-sm-6">
+			<div class="well well-sm row">
+				<h3>Vishnu Singh</h3>
+			</div>
+			<div class="row chat">
+				<div class="panel-body list-group">
+					<a class="list-group-item"><span class="badge">12</span> New</a>
+					<a class="list-group-item"><span class="badge">5</span> Deleted</a> 
+					<a class="list-group-item"><span class="badge">3</span> Warnings</a> 
+					<a class="list-group-item"><span class="badge">12</span> New</a>
+					<a class="list-group-item"><span class="badge">5</span> Deleted</a> 
+					<a class="list-group-item"><span class="badge">3</span> Warnings</a> 
+					<a class="list-group-item"><span class="badge">12</span> New</a>
+					<a class="list-group-item"><span class="badge">5</span> Deleted</a> 
+					<a class="list-group-item"><span class="badge">3</span> Warnings</a>
+					<a class="list-group-item"><span class="badge">12</span> New</a>
+					<a class="list-group-item"><span class="badge">5</span> Deleted</a> 
+					<a class="list-group-item"><span class="badge">3</span> Warnings</a> 
+				</div>
+			</div>
+			<div class="row userinput">
+				<textarea class="col-sm-12" name="" id="" rows="4"></textarea>
+			</div>
+		</div>
+		<div class="col-sm-3 panel panel-primary ">
+			<div class="panel-heading">
+				<h3>Online friends</h3>
+			</div>
+			<div class="panel-body list-group online">
+				<a class="list-group-item"><span class="badge">12</span> New</a>
+				<a class="list-group-item"><span class="badge">5</span> Deleted</a> 
+				<a class="list-group-item"><span class="badge">3</span> Warnings</a> 
+				<a class="list-group-item"><span class="badge">12</span> New</a>
+				<a class="list-group-item"><span class="badge">5</span> Deleted</a> 
+				<a class="list-group-item"><span class="badge">3</span> Warnings</a> 
+				<a class="list-group-item"><span class="badge">12</span> New</a>
+				<a class="list-group-item"><span class="badge">5</span> Deleted</a> 
+				<a class="list-group-item"><span class="badge">3</span> Warnings</a>
+				<a class="list-group-item"><span class="badge">12</span> New</a>
+				<a class="list-group-item"><span class="badge">5</span> Deleted</a> 
+				<a class="list-group-item"><span class="badge">3</span> Warnings</a> 
+			</div>
+		</div>
+	</div>
 </div>
